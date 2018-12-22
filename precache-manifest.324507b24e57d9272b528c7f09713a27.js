e0189c9754778e8153dbb53e8c8112b98dbb1b2b
Nir Bejerano <nir.bejerano@gmail.com>
2018-12-22 15:49:26 +0200

    nir202

---
+++ precache-manifest.324507b24e57d9272b528c7f09713a27.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b2971b4ee4a571627b6",
+    "url": "/static/css/main.9bbc9d2e.chunk.css"
+  },
+  {
+    "revision": "0b2971b4ee4a571627b6",
+    "url": "/static/js/main.0b2971b4.chunk.js"
+  },
+  {
+    "revision": "ee8e772d94b655420fdf",
+    "url": "/static/js/1.ee8e772d.chunk.js"
+  },
+  {
+    "revision": "5649b8e3440b8ffde509",
+    "url": "/static/css/2.0ddfb3a2.chunk.css"
+  },
+  {
+    "revision": "5649b8e3440b8ffde509",
+    "url": "/static/js/2.5649b8e3.chunk.js"
+  },
+  {
+    "revision": "b40e2ddc1d1e123e496d",
+    "url": "/static/js/3.b40e2ddc.chunk.js"
+  },
+  {
+    "revision": "4239f3a21164051345ff",
+    "url": "/static/js/runtime~main.4239f3a2.js"
+  },
+  {
+    "revision": "3c0e7c7ffc5b0c01b36002ff5db310a3",
+    "url": "/static/media/orbitBanner3000.3c0e7c7f.png"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "bf272b934f5a4f28751996b671a67bbe",
+    "url": "/index.html"
+  }
+];
